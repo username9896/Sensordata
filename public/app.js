a5a3538a9e49925a5a8bf59b373c13debbf66270
username9896 <103965166+username9896@users.noreply.github.com>
2023-05-06 04:24:51 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const Sensor_URL = 'https://5a94-14-99-57-10.in.ngrok.io';
+const Sensor_URL = 'http://localhost:5000/send-data';
 
 $('#loginbutton').on('click', () => {
   const username = $('#username').val();
